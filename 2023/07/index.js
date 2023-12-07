858609c4446f1e2d92b7d0973fef484c48e2f00a
Johannes Heinemann <defi@onkeljoe.de>
2023-12-07 22:39:36 +0100

    Day7-2023-Part2

--- 2023/07/index.js
+++ 2023/07/index.js
@@ -1,7 +1,8 @@
 import fs from "fs";
 
 function evaluate(input) {
-  const strengths = "23456789TJQKA";
+  // const strengths = "23456789TJQKA"; // Part 1
+  const strengths = "J23456789TQKA"; // Part 2
   const types = ["11111", "21110", "22100", "31100", "32000", "41000", "50000"];
   let result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   let hex = "";
@@ -10,10 +11,44 @@ function evaluate(input) {
     result[index] += 1;
     hex += index.toString(16);
   }
+  const jokers = result[0];
   const type = types
     .indexOf(result.sort().reverse().join("").substring(0, 5))
     .toString(16);
-  hex = type + hex;
+  let newtype; // Part 2 only
+  switch (jokers) {
+    case 5:
+      newtype = "6";
+      break;
+    case 4:
+      newtype = "6";
+      break;
+    case 3:
+      newtype = type == "4" ? "6" : "5";
+      break;
+    case 2:
+      if (type == "4") {
+        newtype = "6";
+      } else if (type == "2") {
+        newtype = "5";
+      } else newtype = "3";
+      break;
+    case 1:
+      if (type == "5") {
+        newtype = "6";
+      } else if (type == "3") {
+        newtype = "5";
+      } else if (type == "2") {
+        newtype = "4";
+      } else if (type == "1") {
+        newtype = "3";
+      } else newtype = "1";
+      break;
+    default:
+      newtype = type;
+  }
+  // hex = type + hex; // Part 1
+  hex = newtype + hex; // Part 2
   return parseInt(hex, 16);
 }
 
@@ -35,5 +70,7 @@ lines.forEach((line) => {
 games.sort((a, b) => a.strength - b.strength);
 games.forEach((game, index) => (game.value = (index + 1) * game.bid));
 // console.log(games);
-const Part1 = games.reduce((sum, x) => sum + x.value, 0);
-console.log("Part 1: ", Part1);
+// const Part1 = games.reduce((sum, x) => sum + x.value, 0);
+// console.log("Part 1: ", Part1);
+const Part2 = games.reduce((sum, x) => sum + x.value, 0);
+console.log("Part 2: ", Part2);
