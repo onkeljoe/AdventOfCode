b26761458e8d450d549812f980cd72665ae8949a
Johannes Heinemann <defi@onkeljoe.de>
2024-12-01 22:13:10 +0100

    cleanup last year

---
+++ 2023/17/index.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+const lines = fs.readFileSync("./example.txt", "utf-8").split("\r\n");
+// const lines = fs.readFileSync("./input.txt", "utf-8").split("\r\n");
+
+const Part1 = 0;
+const Part2 = 0;
+
+console.log("Part1: ", Part1);
+console.log("Part2: ", Part2);
